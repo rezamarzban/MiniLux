8bfa487d4744cd5ecb97d66d447368d471fd2540
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-19 13:41:38 +0330

    Create make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,7 @@
 #Minimum Linux initramfs and rootfs images builder with networking support from official sources
+
+#Step1: Download latest binary busybox from its official website.
+wget https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64
+
+#Step2: Download latest famous linux Kernel from ubuntu official website.
+wget http://archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux
