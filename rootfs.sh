eb1868573672a7c660535dde1b2790e5393106b0
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-19 15:07:24 +0330

    Create rootfs.sh

--- rootfs.sh
+++ rootfs.sh
@@ -2,5 +2,5 @@
 #Step1: Mount hard drive. Maybe you need change /dev/sda to: /dev/sda1 or /dev/hda or /dev/hda1 or so on.
 mount -t ext2 /dev/sda /root
 
-#Step2: Change root to hard drive and launch the real file system.
+#Step2: Change root to hard drive and launch the real filesystem.
 chroot /root /bin/bash
