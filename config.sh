112ca9cd3930379db44f72a3d5794c674a755932
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-19 16:11:26 +0330

    Create config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,9 @@
 
+#Config ... 
+busybox mount -t proc proc /proc
+busybox mount -t sysfs sys /sys
+busybox mdev -s
+echo nameserver 10.0.2.3>/etc/resolv.conf
+busybox modprobe e1000
+busybox ifconfig eth0 10.0.2.15
+busybox route add default gw 10.0.2.2
