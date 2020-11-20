4d92db30e31bc0d8ae7eefd2b3ea09e8ca68ec36
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-20 21:05:31 +0330

    Create rootfs.sh

--- rootfs.sh
+++ rootfs.sh
@@ -2,10 +2,14 @@
 #Step1: Mount hard drive. Maybe you need change /dev/sda to: /dev/sda1 or /dev/hda or /dev/hda1 or so on.
 mount -t ext2 /dev/sda /root
 
+#Step2: Clean up ...
+umount /proc
+umount /sys
+
 echo 
 echo Welcome to MiniLux real root filesystem.
 echo Run bash /config.sh to configuring system and network.
 echo 
 
-#Step2: Change root to hard drive and launch the real filesystem.
+#Step3: Change root to hard drive and launch the real filesystem.
 chroot /root /bin/bash
