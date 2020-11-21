d365a6d95a318a0cc9da2543cd68a994105e0020
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-21 20:00:20 +0330

    Create OpenGL.sh

---
+++ OpenGL.sh
@@ -0,0 +1,8 @@
+#Install OpenGL, GLX and so on from official sources at MiniLux ...
+#It take less than one hour time to be completed.
+apt-get update
+apt install wget
+cd
+wget 
+http://security.debian.org/debian-security/pool/updates/main/m/mesa/libgl1-mesa-swx11_10.3.2-1+deb8u2_amd64.deb
+apt install /root/*mesa*swx11*.deb
