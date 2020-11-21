17eed5ca0364ef1377bd57026c74f5422d413cc5
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-21 20:10:35 +0330

    Create OpenGL.sh

--- OpenGL.sh
+++ OpenGL.sh
@@ -3,6 +3,5 @@
 apt-get update
 apt install wget
 cd
-wget 
-http://security.debian.org/debian-security/pool/updates/main/m/mesa/libgl1-mesa-swx11_10.3.2-1+deb8u2_amd64.deb
+wget http://security.debian.org/debian-security/pool/updates/main/m/mesa/libgl1-mesa-swx11_10.3.2-1+deb8u2_amd64.deb
 apt install /root/*mesa*swx11*.deb
