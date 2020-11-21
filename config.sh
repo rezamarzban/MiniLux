0123397f78fd9d212b40a67eabf9f2c7fcb24698
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-21 07:46:09 +0330

    Create config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 
 #Config ... 
-bash /root/firstrun.sh
 mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount -t tmpfs tmpfs /run
+bash /root/firstrun.sh
 echo nameserver 10.0.2.3>/etc/resolv.conf
 busybox modprobe e1000
 busybox ifconfig eth0 10.0.2.15
