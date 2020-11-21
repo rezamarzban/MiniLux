d5ff37f77c29d7eb1014269ec5b2d5b2c9887764
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-21 10:44:54 +0330

    Create qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,3 @@
 
 #Run qemu. You can increase qemu virtual machine RAM size by changing 128M to 192M and more.
-qemu-system-x86_64 -cpu max -smp $(nproc) -m 128M -kernel linux -initrd initramfs.gz -hda hard.img -net user -net nic -nographic -append "console=ttyS0" -no-reboot
+qemu-system-x86_64 -cpu max -smp $(nproc) -m 128M -kernel linux -initrd initramfs.gz -hda hard.img hdb -hard2.img -net user -net nic -nographic -append "console=ttyS0" -no-reboot
