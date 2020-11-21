07d56116fd830095de1becee2eca62ec934fbbdf
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-21 12:15:46 +0330

    Create config.sh

--- config.sh
+++ config.sh
@@ -3,6 +3,9 @@
 mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount -t tmpfs tmpfs /run
+busybox mdev -s
+mkswap /dev/sdb
+swapon /dev/sdb
 bash /root/firstrun.sh
 echo nameserver 10.0.2.3>/etc/resolv.conf
 busybox modprobe e1000
