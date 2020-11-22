1250c8edbc50b6f0fd6fded63c6768dda2444ad8
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-22 10:06:03 +0330

    Create OpenGL.sh

--- OpenGL.sh
+++ OpenGL.sh
@@ -1,11 +1,18 @@
 #Install software accelerated OpenGL, GLX and so on from official sources at MiniLux ...
-#It take about half a hour time to be completed.
+echo 
+echo 
+echo It all take about half a hour time to be completed automatically ...
+echo 
+echo 
 apt-get update
-apt install libc-bin wget
+apt install libc-bin wget -y
 cd
 wget http://security.debian.org/debian-security/pool/updates/main/m/mesa/libgl1-mesa-swx11_10.3.2-1+deb8u2_amd64.deb
-apt install /root/*mesa*swx11*.deb
+apt install /root/*mesa*swx11*.deb -y
 cd
 wget http://mirrors.kernel.org/ubuntu/pool/universe/m/mesa-demos/mesa-utils_8.4.0-1_amd64.deb
-apt install /root/mesa*utils*.deb
-echo After exporting DISPLAY and PULSED_... to correct XSDL server, run glxinfo to viewing OpenGL informations. Run glxdemos to testing it.
+apt install /root/mesa*utils*.deb -y
+echo 
+echo OpenGL and GLX installation has been completed. After exporting DISPLAY and PULSED_... to correct XSDL server, run glxinfo to viewing OpenGL informations. Run glxdemos to testing it.
+echo 
+echo 
