13bbc53ec0ac15c9cb864d28819904b8ff5ef40a
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-23 15:55:27 +0330

    Create config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,9 @@ mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount -t tmpfs tmpfs /run
 busybox mdev -s
-mount -t devpts devpts /dev/pts || bash devpts.sh
-mkswap /dev/sdb
-swapon /dev/sdb
+mount -t devpts devpts /dev/pts || bash /devpts.sh
+swapon /dev/sdb || bash /swap.sh
 echo nameserver 10.0.2.3 > /etc/resolv.conf
-busybox modprobe e1000 || bash mod.sh
+busybox modprobe e1000 || bash /mod.sh
 busybox ifconfig eth0 10.0.2.15
 busybox route add default gw 10.0.2.2
