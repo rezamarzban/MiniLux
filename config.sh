8d1622ebd17c208fce30a9934a63c58578b00268
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-06-18 10:27:10 +0430

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 
 #Config ... 
 reset
-cd
 mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount -t tmpfs tmpfs /run
@@ -12,3 +11,4 @@ echo nameserver 10.0.2.3 > /etc/resolv.conf
 busybox modprobe e1000 || bash /mod.sh
 busybox ifconfig eth0 10.0.2.15
 busybox route add default gw 10.0.2.2
+cd
